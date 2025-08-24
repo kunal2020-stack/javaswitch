4e13e98af627528c14cb1125b8119cf43bdedabb
kunal2020-stack <kunal2115kumar@gmail.com>
2025-08-24 17:56:14 +0530

    java tutorial for beginers video 4

---
+++ jswitch/src/jswitch/Jtbv4.java
@@ -0,0 +1,11 @@
+package jswitch;
+
+public class Jtbv4 {
+	public static void main (String []args) {
+		int num1 = 1;
+		int num2 = 3;
+		int result = num1+num2;
+		System.out.println(result);
+	}
+
+}

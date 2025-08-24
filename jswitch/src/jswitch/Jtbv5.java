52cd18a1d633b6faebd7baefbf865a404dd83c69
kunal2020-stack <kunal2115kumar@gmail.com>
2025-08-24 19:43:04 +0530

    literals

--- jswitch/src/jswitch/Jtbv5.java
+++ jswitch/src/jswitch/Jtbv5.java
@@ -1,13 +1,13 @@
 package jswitch;
 
-public class Jtbv4 {
+public class Jtbv5 {
 	public static void main (String []args) {
 		int num1 = 1;
 		int num2 = 3;
 		int result = num1+num2;
 		System.out.println(result);
 		
-//		Jtbv5 -> variable
+//		Jtbv6 -> variable
 //		int num1 = 9;
 		byte by = 127;
 		short sh = 558;
@@ -26,6 +26,14 @@ public class Jtbv4 {
 		System.out.println(d);
 		System.out.println(c);
 		System.out.println(b);
+		
+//		Jbtv7
+		
+//		value assigned to the variable is also known as literals
+		char cc = 'c';
+		cc +=1;
+		cc++;
+		System.out.println(cc);
 	}
 
 }

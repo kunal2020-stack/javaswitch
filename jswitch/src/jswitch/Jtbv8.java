9a10f9c8f9c3925e307621c215001758e6604c2e
kunal2020-stack <kunal2115kumar@gmail.com>
2025-08-24 20:05:49 +0530

    java tutorial v8 -> type conversion,type casting and type promotion

---
+++ jswitch/src/jswitch/Jtbv8.java
@@ -0,0 +1,32 @@
+package jswitch;
+
+public class Jtbv8 {
+	public static void main (String []args) {
+		
+//		below section is the example of the type conversion as we are not explicitly converting byte to integer but the java itself
+		int bbbb = 124;
+		int aaaa = bbbb;
+		System.out.println(aaaa);
+		
+//		below section is the example fo the type casting but the value of a is more than the range of byte so it will perform
+//			modulo /reminder operation to assign the value 257 %max range of byte value
+		int aaa = 257;
+		int bbb = (byte) aaa;
+		System.out.println(bbb);
+		
+//		below section is the example of the type casting as we are explicitly converting int into byte
+		int aa = 127;
+		byte bb = (byte)aa;
+		System.out.println(bb);
+		
+
+		
+		//		below is the example of the type promotion
+		byte a = 10;
+		byte b = 30;
+		
+		int result = a*b;
+		System.out.println(result);
+	}
+
+}

124b873a003e9378c616d0e70d5d3de06221a457
kunal2020-stack <kunal2115kumar@gmail.com>
2025-08-25 23:48:41 +0530

    oops methods

---
+++ jswitch/src/jswitch/Jtbv24.java
@@ -0,0 +1,27 @@
+package jswitch;
+
+class Computer{
+	
+	public void playMusic() {
+		System.out.println("Playing Music....");
+	}
+	
+	public String getMeApen(int cost) {
+		if (cost >=10)
+			return "PEN";
+		return "Nothing";
+	}
+}
+
+public class Jtbv24 {
+	public static void main(String []args) {
+		
+		Computer obj = new Computer();
+		obj.playMusic();
+		
+		String str = obj.getMeApen(2);
+		System.out.println(str);
+	}
+	
+
+}

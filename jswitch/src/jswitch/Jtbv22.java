0631e016308e42ffe89cf914d53fcebad50d0ce8
kunal2020-stack <kunal2115kumar@gmail.com>
2025-08-26 00:10:38 +0530

    Method overloading

--- jswitch/src/jswitch/Jtbv22.java
+++ jswitch/src/jswitch/Jtbv22.java
@@ -1,9 +1,24 @@
 package jswitch;
 
 class Calculator{
+	
+//	just below is normal example
 	public int add (int n1,int n2) {
 		int r = n1+n2;
 		return r;
+	}
+	
+//	just below is the example of the method overloading
+	public int sum(int n1,int n2) {
+		return n1+n2;
+	}
+	
+	public int sum(int n1,int n2, int n3) {
+		return n1+n2+n3;
+	}
+	
+	public double sum(int n1,double n3) {
+		return n1+n3;
 	}
 }
 
@@ -15,7 +30,13 @@ public class Jtbv22 {
 		Calculator calc = new Calculator();
 		int result = calc.add(num1, num2);
 		System.out.println(result);
+		System.out.println("-------- SEPARATOR TEXT ---------");
 		
+		Calculator s1= new Calculator();
+		
+		System.out.println(s1.sum(1, 2));
+		System.out.println(s1.sum(1, 2, 3));
+		System.out.println(s1.sum(1, 2.9));
 	}
 
 }

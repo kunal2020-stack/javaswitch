4e6ac96bc2189d0612f4c0624401d7001db8f2a2
kunal2020-stack <kunal2115kumar@gmail.com>
2025-08-26 00:42:09 +0530

    Array v28

---
+++ jswitch/src/jswitch/Jtbv28.java
@@ -0,0 +1,23 @@
+package jswitch;
+
+public class Jtbv28 {
+	public static void main (String []args) {
+		int []num = {1,2,3,4,5};
+		for(int i=0;i<5;i++) {
+			System.out.println(num[i]);
+		}
+		
+		System.out.println("----- Line Separator ------");
+		
+		int num1[] = new int[4];
+		num1[0] = 1;
+		num1[1] = 2;
+		num1[2] = 33;
+		num1[3] = 4;
+		
+		for(int i=0;i<4;i++) {
+			System.out.println(num1[i]);
+		}
+	}
+
+}

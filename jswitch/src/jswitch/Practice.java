e0ddec0eaf8eb9f03c8d303d28340d1ed2879ae3
kunal2020-stack <kunal2115kumar@gmail.com>
2025-08-25 23:00:25 +0530

    Oops concept related note v21

---
+++ jswitch/src/jswitch/Practice.java
@@ -0,0 +1,12 @@
+package jswitch;
+
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the file where i will be practising the code");
+
+	}
+
+}

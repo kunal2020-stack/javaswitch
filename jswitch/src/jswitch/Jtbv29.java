a40a93c6477f5053ed46ff6de782db2b27756a11
kunal2020-stack <kunal2115kumar@gmail.com>
2025-08-26 22:40:49 +0530

    multi-dim array-2d

---
+++ jswitch/src/jswitch/Jtbv29.java
@@ -0,0 +1,37 @@
+package jswitch;
+
+public class Jtbv29 {
+	public static void main (String []args) {
+		
+		int nums[][] = new int[3][4];
+		
+//		assigning random value to the array using inbuilt function
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<4;j++) {
+				nums[i][j] = (int) (Math.random() *10); //function returns value between 0.0 and 1.0(not included)
+			}
+		}
+		
+		
+		
+		
+//		printing values using normal for loop
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<4;j++) {
+				System.out.print(nums[i][j]+" ");
+			}
+			System.out.print("\n");
+		}
+		
+		System.out.println("-----SEPARATOR TEXT-----");
+		
+//		printing the value using for-each loop
+		for(int n[]:nums) {
+			for(int m:n) {
+				System.out.print(m+" ");
+			}
+			System.out.println("");
+		}
+	}
+
+}

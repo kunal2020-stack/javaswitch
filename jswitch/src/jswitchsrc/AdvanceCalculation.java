245fe490a09e3b0071e00a7c5e37ee1b2c17aaed
kunal2020-stack <kunal2115kumar@gmail.com>
2025-08-31 23:24:49 +0530

    inheritance

---
+++ jswitch/src/jswitchsrc/AdvanceCalculation.java
@@ -0,0 +1,12 @@
+package jswitchsrc;
+
+public class AdvanceCalculation extends BasicCalculator {
+    int mul(int a, int b) {
+        return a * b;
+    }
+
+    int div(int a, int b) {
+        return a / b;
+    }
+    
+}
--- jswitch/src/jswitchsrc/Calc.java
+++ jswitch/src/jswitchsrc/Calc.java
@@ -1,16 +1,18 @@
 package jswitchsrc;
 
-public class Calc extends BasicCalculator {
+public class Calc extends AdvanceCalculation {
 
 	public static void main(String[] args) {
 		Calc c1 = new Calc();
 		int add = c1.add(1, 2);
 		int sub = c1.sub(2, 1);
-	// 	int mul = c1.mul(2, 2);
-	// 	int div = c1.div(1, 2);
+		int mul = c1.mul(2, 2);
+		double div = c1.div(10, 2);
 		
 		System.out.println("Addtion: "+add);
-		System.out.println("Subtraction: "+sub); //there
+		System.out.println("Subtraction: "+sub);
+		System.out.println("Multiplication: "+mul);
+		System.out.println("Division: "+div);
 	}
 
 }
